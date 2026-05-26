a2e55cc821d118282e8e724b855d0fa7b55947dc
mostandmore4u <mostandmore4u@gmail.com>
2026-05-26 21:20:25 +0530

    roboshop shell

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ DOMAIN_NAME="subbudevops.online"
 
 for instance in $@
 do 
-    echo "Launching instance: $instance"
+    echo "Launching instance: "$instance"
     INSTANCE_ID=$(aws ec2 run-instances \
     --image-id ami-0220d79f3f480ecf5 \
     --instance-type t3.micro \
