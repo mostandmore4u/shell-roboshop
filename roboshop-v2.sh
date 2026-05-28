8b2c8900d587aeb635af2bd7f9bcb00392bfb335
mostandmore4u <mostandmore4u@gmail.com>
2026-05-28 11:24:01 +0530

    api doc added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -26,14 +26,13 @@ fi
 
 get_instance_id(){
     name=$1
-    aws ec2 describe-instances --filters \
-    "Name=tag:Name,Values=roboshop-$instance" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
+    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$instance" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
 }
 
 for instance in $@
 do
     get_instance_id $instance
-    if [ ACTION == "create"]; then
+    if [ $ACTION == "create"]; then
       if [ $INSTANCE_ID == "None"]; then
         echo "Launching Instance: roboshop-$instance"
         INSTANCE_ID=$(aws ec2 run-instances \
