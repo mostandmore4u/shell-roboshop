ffde153ac64ca413e0c36bfde4fee5a2af5306b1
mostandmore4u <mostandmore4u@gmail.com>
2026-05-29 21:39:40 +0530

    api doc added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -39,7 +39,7 @@ do
     if [ $ACTION == "create" ]; then
       if [ "$INSTANCE_ID" == "None" ]; then
         echo "Launching Instance: roboshop-$instance"
-        INSTANCE_ID=$(/usr/local/bin/aws ec2 run-instances \
+        INSTANCE_ID=$(aws ec2 run-instances \
         --image-id $AMI_ID \
         --instance-type t3.micro \
         --security-groups "roboshop-common" "roboshop-$instance" \
@@ -55,7 +55,7 @@ do
 
       # update R53 record
       if [ "$instance" == "frontend" ]; then
-            IP=$(/usr/local/bin/aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+            IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
             --query 'Reservations[*].Instances[*].PublicIpAddress' \
             --output text
           )
